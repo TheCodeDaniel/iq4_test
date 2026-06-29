ffaa956bc0374949b912b63884f7850d6ea7bc29
Daniel <thecodedaniel@gmail.com>
2026-06-29 10:33:58 +0100

    feat: Implement PIN validation and feedback in CustomPinScreen

--- lib/views/pin_lock_view.dart
+++ lib/views/pin_lock_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iq_test/widgets/custom_pin_screen.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class PinLockView extends StatefulWidget {
   const PinLockView({super.key});
@@ -11,6 +12,18 @@ class PinLockView extends StatefulWidget {
 class _PinLockViewState extends State<PinLockView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: CustomPinScreen()));
+    return Scaffold(
+      body: SafeArea(child: CustomPinScreen(onPinComplete: _handlePinCheck)),
+    );
+  }
+
+  void _handlePinCheck(String value) {
+    if (value == "123456") {
+      // PIN is correct, navigate to the next screen or perform desired action
+      Fluttertoast.showToast(msg: "PIN is correct! Proceeding to the next screen.");
+    } else {
+      // PIN is incorrect, show an error message
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Incorrect PIN. Please try again.')));
+    }
   }
 }
